923abf8a4d983514a1c579919701c0faa43d5594
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-03-25 21:49:39 -0300

    feat: adds main page layout

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from './styles';
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer />
+        <MenuContainer />
+      </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
   );
 }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -5,4 +5,25 @@ const isAndroid = Platform.OS === 'android';
 
 export const Container = styled.SafeAreaView`
   padding-top: ${isAndroid ? StatusBar.currentHeight : 0}px;
+  flex: 1;
+  background: #fafafa;
 `;
+
+export const CategoriesContainer = styled.View`
+  height: 72px;
+  margin-top: 34px;
+  border: 1px solid red;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+  border: 1px solid blue;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  border: 1px solid green;
+  background: #fff;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
