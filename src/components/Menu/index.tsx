4408932d22278d2aa74d8c5e38fe7a196509e277
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-03-26 21:25:05 -0300

    feat: implements menu section

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,39 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+import { products } from '../../../mocks/products';
+import { formatCurrency } from '../../utils/format-currency';
+import { PlusCircle } from '../Icons/PlusCircle';
 import { Text } from '../Text';
-import { Container } from './styles';
+import { AddToCartButton, Product, ProductDetails, ProductImage, Separator } from './styles';
 
 export function Menu() {
   return (
-    <Container>
-      <Text>Menu</Text>
-    </Container>
+    <FlatList
+      data={products}
+      keyExtractor={(product) => product._id}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      ItemSeparatorComponent={Separator}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage source={{ uri: product.imagePath }} />
+          <ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
   );
 }
