0ad0912b5f8ae9130676cf4a137d1131fde0ed70
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-03-27 21:50:35 -0300

    feat: renders header content conditionaly

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,27 @@
 import { Text } from '../Text';
 import { Container } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  selectedTable: string;
+}
+
+export function Header({ selectedTable }: HeaderProps) {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>
-        Bem-vindo(a) ao
-      </Text>
-      <Text size={24} weight="700">
-        WAITER<Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable ? (
+        <>
+          <Text size={14} opacity={0.9}>
+            Bem-vindo(a) ao
+          </Text>
+          <Text size={24} weight="700">
+            WAITER<Text size={24}>APP</Text>
+          </Text>
+        </>
+      ) : (
+        <Text size={20} weight="700">
+          Mesa {selectedTable}
+        </Text>
+      )}
     </Container>
   );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -22,7 +22,7 @@ export function Main() {
   return (
     <>
       <Container>
-        <Header />
+        <Header selectedTable={selectedTable} />
 
         <CategoriesContainer>
           <Categories />
